cf4ee4bf54bc8c2e1d8accc6bffc0cca3958f0fb
m4x1m1l14n <sepental.patrik@gmail.com>
2019-11-26 15:34:03 +0100

    Basic event manipulation helpers implemented. Multiple wait missing .

--- System/src/System/Threading/EventWaitHandle.cpp
+++ System/src/System/Threading/EventWaitHandle.cpp
@@ -6,6 +6,9 @@
 
 #else
 
+#include <pthread.h>
+#include <sys/time.h>
+
 typedef void* HANDLE;
 typedef uint32_t DWORD;
 typedef int BOOL;
@@ -13,15 +16,35 @@ typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
+using namespace System::Threading;
+
 namespace Linux
 {
 	struct EventObject
 	{
-
+		pthread_mutex_t Mutex;
+		pthread_cond_t Condition;
+		bool Signaled;
+		bool ManualReset;
 	};
 
 	HANDLE CreateEvent(void* pEventAttributes, BOOL bManualReset, BOOL bInitialState, const wchar_t* lpName)
 	{
+		auto pEvent = new EventObject();
+
+		if (pthread_cond_init(&pEvent->Condition, 0) == 0)
+		{
+			if (pthread_mutex_init(&pEvent->Mutex, 0) == 0)
+			{
+				pEvent->ManualReset = bManualReset ? true : false;
+				pEvent->Signaled = bInitialState ? true : false;
+
+				return pEvent;
+			}
+		}
+
+		delete pEvent;
+
 		return nullptr;
 	}
 
@@ -31,34 +54,163 @@ namespace Linux
 		{
 			auto pEvent = reinterpret_cast<EventObject*>(hObject);
 
+			pthread_mutex_destroy(&pEvent->Mutex);
+			pthread_cond_destroy(&pEvent->Condition);
+
 			delete pEvent;
 		}
 
 		return TRUE;
 	}
 
-	BOOL SetEvent(HANDLE hEvent)
+	BOOL SetSignaledState(HANDLE hEvent, bool signaled)
 	{
 		if (hEvent == nullptr)
 		{
-
+			// TODO SetLastError()
+			return FALSE;
 		}
 
-		BOOL fResult = FALSE;
-
+		BOOL bResult = FALSE;
 		auto pEvent = reinterpret_cast<EventObject*>(hEvent);
+
+		int err = pthread_mutex_lock(&pEvent->Mutex);
+		if (err == 0)
+		{
+			pEvent->Signaled = signaled;
+			
+			bResult = TRUE;
+
+			pthread_mutex_unlock(&pEvent->Mutex);
+		}
+
+		pthread_cond_broadcast(&pEvent->Condition);
 		
-		return TRUE;
+		return bResult;
+	}
+
+	BOOL SetEvent(HANDLE hEvent)
+	{
+		SetSignaledState(hEvent, true);
 	}
 
 	BOOL ResetEvent(HANDLE hEvent)
 	{
-		return TRUE;
+		SetSignaledState(hEvent, false);
 	}
 
 	DWORD WaitForSingleObject(HANDLE hEvent, DWORD milliseconds)
 	{
-		return 0;
+		if (hEvent == nullptr)
+		{
+			return EventWaitHandle::WaitFailed;
+		}
+
+		auto pEvent = reinterpret_cast<EventObject*>(hEvent);
+
+		int err = 0;
+
+        if (milliseconds == 0)
+		{
+            err = pthread_mutex_trylock(&pEvent->Mutex);
+            if (err == EBUSY)
+			{
+                return EventWaitHandle::WaitTimeout;
+            }
+        }
+		else
+		{
+            err = pthread_mutex_lock(&pEvent->Mutex);
+        }
+
+		// Obtaining lock failed with error
+		if (err != 0)
+		{
+			return EventWaitHandle::WaitFailed;
+		}
+
+		uint32_t result = EventWaitHandle::WaitFailed;
+
+		// Event already signaled, no need to wait anymore
+		if (pEvent->Signaled)
+		{
+			// We have manual reset event in signaled state
+			// so do nothing
+			if (pEvent->ManualReset)
+			{
+			
+			}
+			// For AutoReset event we have to reset its state on successfull wait
+			else
+			{
+				pEvent->Signaled = false;
+			}
+			
+			// Set successfull wait result
+			result = EventWaitHandle::WaitObject0;
+		}
+		// Event is not signaled, so we have to wait
+		else
+		{
+			// Infinite wait requested
+			if (milliseconds == EventWaitHandle::Infinite)
+			{
+				// Wait until event signaled or error
+				do
+				{
+					err = pthread_cond_wait(&pEvent->Condition, &pEvent->Mutex);
+					
+				} while (err == 0 && !pEvent->Signaled);
+			}
+			// Timed wait requested
+			else
+			{
+				timespec ts;
+				timeval tv;
+
+				gettimeofday(&tv, nullptr);
+
+				uint64_t nanoseconds = ((uint64_t)tv.tv_sec) * 1000 * 1000 * 1000 + milliseconds * 1000 * 1000 + ((uint64_t)tv.tv_usec) * 1000;
+
+				ts.tv_sec = nanoseconds / 1000 / 1000 / 1000;
+				ts.tv_nsec = (long) (nanoseconds - ((uint64_t)ts.tv_sec) * 1000 * 1000 * 1000);
+
+				// Wait until event signaled, timeout or error
+				do
+				{
+					err = pthread_cond_timedwait(&pEvent->Condition, &pEvent->Mutex, &ts);
+					
+				} while (err == 0 && !pEvent->Signaled);
+			}
+
+			// Handle wait result
+			if (err == 0)
+			{
+				// Reset event state if AutoReset event
+				if (!pEvent->ManualReset)
+				{
+					pEvent->Signaled = false;
+				}
+
+				result = EventWaitHandle::WaitObject0;
+			}
+			else if (err == ETIMEDOUT)
+			{
+				result = EventWaitHandle::WaitTimeout;
+			}
+			else
+			{
+				result = EventWaitHandle::WaitFailed;
+			}
+		}
+
+		err = pthread_mutex_unlock(&pEvent->Mutex);
+		if (err != 0)
+		{
+			// TODO Dont know if this should return WaitFailed?
+		}
+        
+        return result;
 	}
 
 	DWORD WaitForMultipleObjects(DWORD nCount, const HANDLE* pHandles, BOOL bWaitAll, DWORD milliseconds)
@@ -175,16 +327,14 @@ namespace System
 			{
 				auto dwCount = static_cast<DWORD>(waitHandles.size());
 
-				auto *pHandles = new HANDLE[dwCount];
+				auto pHandles = std::make_unique<HANDLE[]>(dwCount);
 
 				for (DWORD i = 0; i < dwCount; ++i)
 				{
-					pHandles[i] = static_cast<HANDLE>(*waitHandles[i]);
+					pHandles[i] = static_cast<void*>(*waitHandles[i]);
 				}
 
-				ret = static_cast<int>(WaitForMultipleObjects(dwCount, pHandles, FALSE, milliseconds));
-
-				delete[] pHandles;
+				ret = static_cast<int>(WaitForMultipleObjects(dwCount, pHandles.get(), FALSE, milliseconds));
 			}
 
 			return ret;
@@ -203,16 +353,14 @@ namespace System
 			{
 				auto dwCount = static_cast<DWORD>(waitHandles.size());
 
-				auto *pHandles = new HANDLE[dwCount];
+				auto pHandles = std::make_unique<HANDLE[]>(dwCount);
 				
 				for (DWORD i = 0; i < dwCount; ++i)
 				{
-					pHandles[i] = static_cast<HANDLE>(*waitHandles[i]);
+					pHandles[i] = static_cast<void*>(*waitHandles[i]);
 				}
 
-				ret = (WaitForMultipleObjects(dwCount, pHandles, TRUE, milliseconds) == EventWaitHandle::Waitobject<0>());
-
-				delete[] pHandles;
+				ret = (WaitForMultipleObjects(dwCount, pHandles.get(), TRUE, milliseconds) == EventWaitHandle::Waitobject<0>());
 			}
 
 			return ret;
