538c7c0dd1cd898a0ebd38aeb21d76f502f6f918
m4x1m1l14n <sepental.patrik@gmail.com>
2020-01-17 13:03:40 +0100

    Removed unused defines

--- System/include/System/Net/IPC/IpcDefines.hpp
+++ System/include/System/Net/IPC/IpcDefines.hpp
@@ -18,30 +18,21 @@ namespace System
 				| Process ID | Instance ID |
 			*/
 			typedef std::uint64_t IpcClientId;
-
 			typedef std::uint64_t IpcMessageId;
-
 			typedef std::uint32_t IpcMessageLength;
-
-			typedef std::uint32_t IpcFrameLength;
 
 			const IpcMessageId IpcRegisterMessageId = 0;
 
-			const char IpcFrameStart = -86; // 0xAA in hex
-			const char IpcMessageStart = -86;
+			const char IpcMessageStart = -86; // 0xAA in hex
+
 			const std::uint64_t IpcServerRequestFlag = 0x8000000000000000;
 			const std::uint64_t IpcClientRequestFlag = 0x0000000000000000;
+			
 			// IPC message header
 			//	1 byte 0xAA - frame start identificator
 			//	4 bytes		- length of message payload
 			//	8 bytes		- message id
 			const auto IpcMessageHeaderSize = sizeof(IpcMessageStart) + sizeof(IpcMessageLength) + sizeof(IpcMessageId);
-			const auto IpcFrameHeaderSize = sizeof(IpcFrameStart) + sizeof(IpcFrameLength);
-
-			namespace details
-			{
-				//typedef std::map<const IpcMessageId, IpcRequest_ptr> RequestsQueue;
-			}
 		}
 	}
 }
